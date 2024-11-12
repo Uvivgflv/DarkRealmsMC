745292cbcc67271a03ed07b88d95527d8b74b1c3
Uvivgflv <122619968+Uvivgflv@users.noreply.github.com>
2024-11-12 15:58:17 +0500

    Delete usless.js

--- items_modifications.js
+++
@@ -1,7 +0,0 @@
-ItemEvents.modification(event => {
-    event.modify('kebejs:fiery_scraps', item => {
-      item.maxStackSize = 64
-      item.fireResistant = true
-      item.rarity = 'RARE'
-    })
-  })
