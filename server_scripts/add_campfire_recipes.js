3a9f2b9d92fbc4affb55eb24803f04a80ce523eb
Uvivgflv <122619968+Uvivgflv@users.noreply.github.com>
2024-11-26 03:18:34 +0500

    Revert "Some work been completed (#15)"
    This reverts commit 1d38074157747e4872334a691fe1005163ac2c00.

--- server_scripts/add_campfire_recipes.js
+++
@@ -1,4 +0,0 @@
-ServerEvents.recipes(event =>{
-    //early bricks from clay
-    event.campfireCooking('minecraft:brick', 'minecraft:clay_ball').xp(0.1).cookingTime(850)
-})
--- server_scripts/irons_spells/remove_recipes.js
+++
@@ -1,6 +0,0 @@
-ServerEvents.recipes(event =>{
-    event.remove({id:'irons_spellbooks:diamond_spell_book'})
-    event.remove({id:'irons_spellbooks:gold_spell_book'})
-    event.remove({id:'irons_spellbooks:netherite_spell_book'})
-    event.remove({id:'irons_spellbooks:dragonskin_spell_book'})
-})
