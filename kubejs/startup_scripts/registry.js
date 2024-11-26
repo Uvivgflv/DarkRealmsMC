1b41bf0dc8a9bfb826936dd38adc2787fadab17f
Uvivgflv <122619968+Uvivgflv@users.noreply.github.com>
2024-11-27 01:02:20 +0500

    add items registry

--- kubejs/startup_scripts/registry.js
+++ kubejs/startup_scripts/registry.js
@@ -11,9 +11,15 @@ StartupEvents.registry('item', event => {
     event.create('diamond_weapon_upgrade').displayName('Алмазное улучшение оружия').maxStackSize(16)
     event.create('netherite_weapon_upgrade').displayName('Незеритовое улучшение оружия').maxStackSize(16)
     //spellbooks
-
-    event.create('diamond_spell_book_part').displayName('Основы Книги заклинаний').unstackable().rarity('UNCOMMON')
-    event.create('golden_spell_book_part').displayName('Основы Зачарованной книги заклинаний').unstackable().rarity('RARE')
+    event.create('diamond_spell_book_part').displayName('Основы Книги заклинаний').unstackable().rarity('RARE')
+    event.create('golden_spell_book_part').displayName('Основы Зачарованной книги заклинаний').unstackable().rarity('UNCOMMON')
     event.create('netherite_spell_book_part').displayName('Основы Древнего трактата').unstackable().rarity('EPIC')
+    //magic materials
+    event.create('amethyst_dust').displayName('Аметистовая пыль')
+    event.create('magic_powder').displayName('Ускоряющая пыль')
+    event.create('enchanted_golden_plate').displayName('Зачарованный золотой лист')
+    event.create('enchanted_diamond').displayName('Зачарованный алмаз')
+    //misc materials
+    event.create('plant_fabric').displayName('Растительное волокно')
 
 })
