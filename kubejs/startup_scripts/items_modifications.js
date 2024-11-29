d821e21bb3d62173935b5c2e80ed1372c9dec74d
Uvivgflv <122619968+Uvivgflv@users.noreply.github.com>
2024-11-29 14:34:00 +0500

    modify templates

--- kubejs/startup_scripts/items_modifications.js
+++ kubejs/startup_scripts/items_modifications.js
@@ -8,5 +8,29 @@ ItemEvents.modification(event => {
     event.modify(/^minecraft:.*_smithing_template/, item =>{
       item.maxStackSize = 16
     })
+
+    event.modify('kubejs:enchanted_diamond', item =>{
+      item.glow(true)
+    })
+    event.modify('kubejs:enchanted_golden_plate', item =>{
+      item.glow(true)
+    })
+    
+    event.modify('forbidden_arcanus:darkstone_upgrade_smithing_template', item =>{
+      item.maxStackSize(16)
+    })
+    event.modify('quark:smithing_template_rune', item =>{
+      item.maxStackSize(16)
+    })
+    event.modify('cataclysm:ignitium_upgrade_smithing_template', item =>{
+      item.maxStackSize(16)
+    })
+    event.modify('born_in_chaos_v1:dark_upgrade', item =>{
+      item.maxStackSize(16)
+    })
+    event.modify('biomemakeover:cladding_upgrade_smithing_template', item =>{
+      item.maxStackSize(16)
+    })
+
   })
   
